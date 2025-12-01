3b4f52b651254a021672c5d427b10438248cc76e
RenatoDidier <renatodidierf@gmail.com>
2025-12-01 13:48:19 -0300

    feat: Configure to build WebApp of my project in Portal Azure

---
+++ deploy/KeepImprovingStack.cs
@@ -0,0 +1,57 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.DBforMySQL;
+using Pulumi.AzureNative.DBforMySQL.Inputs;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Web;
+using Pulumi.AzureNative.Web.Inputs;
+
+namespace KeepImproving.Deploy;
+
+public static class PulumiNameFormatter
+{
+    public static string Format(string name)
+    {
+        return name.ToLower().Replace(".deploy", string.Empty).Replace('.', '-');
+    }
+}
+public class KeepImprovingStack : Stack
+{
+    public KeepImprovingStack() {
+
+        var stack = Pulumi.Deployment.Instance.StackName;
+        var projectName = PulumiNameFormatter.Format(Pulumi.Deployment.Instance.ProjectName);
+
+        var resourceGroup = ResourceGroup.Get("rg-keepimproving-dev-brs", "/subscriptions/39a689b6-9fb4-4598-a6d7-9bd1994848ab/resourceGroups/rg-keepimproving-dev-brs");
+
+        var appServicePlan = new AppServicePlan("appServicePlan", new()
+        {
+            Name = $"asp-{projectName}-{stack}",
+            Kind = "linux",
+            Location = resourceGroup.Location,
+            ResourceGroupName = resourceGroup.Name,
+            Sku = new SkuDescriptionArgs
+            {
+                Capacity = 1,
+                Family = "B",
+                Name = "B1",
+                Size = "B1",
+                Tier = "Basic",
+            },
+            Reserved = true
+        });
+
+        var webApp = new WebApp("webApp", new()
+        {
+            Kind = "linux,app",
+            Location = resourceGroup.Location,
+            ResourceGroupName = resourceGroup.Name,
+            Name = $"app-{projectName}-{stack}-api",
+            ServerFarmId = appServicePlan.Id,
+            SiteConfig = new SiteConfigArgs
+            {
+                AutoHealEnabled = true,
+                HealthCheckPath = "/health",
+            }
+        });
+    }
+}
--- deploy/Program.cs
+++ deploy/Program.cs
@@ -1,40 +1,10 @@
-﻿using Pulumi;
-using Pulumi.AzureNative.Resources;
-using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
-using System.Collections.Generic;
+﻿using System.Threading.Tasks;
+using KeepImproving.Deploy;
 
-return await Pulumi.Deployment.RunAsync(() =>
+internal class Program
 {
-    // Create an Azure Resource Group
-    var resourceGroup = new ResourceGroup("rg-keepimproving-dev-brs");
-
-    // Create an Azure resource (Storage Account)
-    var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+    private static async Task<int> Main(string[] args)
     {
-        ResourceGroupName = resourceGroup.Name,
-        Sku = new SkuArgs
-        {
-            Name = SkuName.Standard_LRS
-        },
-        Kind = Kind.StorageV2
-    });
-
-    var storageAccountKeys = ListStorageAccountKeys.Invoke(new ListStorageAccountKeysInvokeArgs
-    {
-        ResourceGroupName = resourceGroup.Name,
-        AccountName = storageAccount.Name
-    });
-
-    var primaryStorageKey = storageAccountKeys.Apply(accountKeys =>
-    {
-        var firstKey = accountKeys.Keys[0].Value;
-        return Output.CreateSecret(firstKey);
-    });
-
-    // Export the primary key of the Storage Account
-    return new Dictionary<string, object?>
-    {
-        ["primaryStorageKey"] = primaryStorageKey
-    };
-});
+        return await Pulumi.Deployment.RunAsync<KeepImprovingStack>();
+    }
+}
