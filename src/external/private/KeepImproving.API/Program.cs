f048456db6f10aedfda884cd6f2b12fc0cc0c2d2
RenatoDidier <renatodidierf@gmail.com>
2025-12-23 19:36:38 -0300

    feat: Add Logger to analyse migration into project

--- src/external/private/KeepImproving.API/Program.cs
+++ src/external/private/KeepImproving.API/Program.cs
@@ -3,6 +3,7 @@ using FastEndpoints.Swagger;
 using KeepImproving.API.Extensions;
 using KeepImproving.Infra.Contexts;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +14,34 @@ builder.Services.ConfigureDatabase(builder.Configuration)
 
 WebApplication app = builder.Build();
 
-using (IServiceScope? scope = app.Services.CreateScope())
+
+using (IServiceScope scope = app.Services.CreateScope())
 {
+    ILogger<Program> logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
     AppDbContext? db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    var stopWatch = Stopwatch.StartNew();
+
+    try
+    {
+        logger.LogInformation("Starting EF Core migrations...");
+        db.Database.Migrate();
+        stopWatch.Stop();
+
+        logger.LogInformation(
+            "EF Core migrations completed successfully in {ElapsedMs} ms",
+            stopWatch.ElapsedMilliseconds
+        );
+
+    } 
+    catch (Exception ex)
+    {
+        logger.LogCritical(
+            ex,
+            "EF Core migrations failed after {ElapsedMs} ms",
+            stopWatch.ElapsedMilliseconds
+        );
+        throw;
+    }
 }
 
 app.UseHttpsRedirection();
